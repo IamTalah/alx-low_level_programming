69fccc40ae071e45858d0111a3c0c726b2768d69
IamTalah <viktormbewe@gmail.com>
2023-02-20 23:50:01 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Opening point
+ * Description - prints the alphabet, in lowercase, followed by a new line.
+ * Return: Nothing
+ **/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
