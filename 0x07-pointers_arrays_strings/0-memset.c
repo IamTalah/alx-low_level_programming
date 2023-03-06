f181a57383c9b2790e12e5bd6b01fc31e359f4d4
IamTalah <viktormbewe@gmail.com>
2023-03-06 01:30:05 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	char *p = s;
 
-	for (i = 0; n > 0; i++, n--)
+	while (n-- > 0)
 	{
-		s[i] = b;
+		*p++ = b;
 	}
-
 	return (s);
 }
 
